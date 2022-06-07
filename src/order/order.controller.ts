980b4e05ea4eda2d07ac9511a3ef3efe6c2d4e42
张新永 <13271150671@wo.cn>
2022-06-08 00:32:55 +0800

    feat: aggregate

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -32,27 +32,31 @@ export class OrderController {
   @Get('find')
   async find() {
     // find 一个参数对象是条件 第二次参数返回字段
-    const data = await this.orderModel.aggregate([
-      {
-        $lookup: {
-          from: 'orderitems', // 关联的表
-          localField: 'order_id', // order表的某个字段
-          foreignField: 'order_id', // order_item表中的某个字段
-          as: 'items', // order_item查询数据的放置字段
-        },
-      },
-      // 还可以继续关联其他表
-      // {},
-      // 过滤条件
-      {
-        $match: {
-          uid: {
-            // $lt: 102,
-            $lte: 102,
+    const data = await this.orderModel
+      .aggregate([
+        {
+          $lookup: {
+            from: 'orderitems', // 关联的表
+            localField: 'order_id', // order表的某个字段
+            foreignField: 'order_id', // order_item表中的某个字段
+            as: 'items', // order_item查询数据的放置字段
           },
         },
-      },
-    ]);
+        // 还可以继续关联其他表
+        // {},
+        // 过滤条件
+        // {
+        //   $match: {
+        //     uid: {
+        //       // $lt: 102,
+        //       $lte: 102,
+        //     },
+        //   },
+        // },
+      ])
+      // .skip(5) // 跳过
+      // .limit(2) // 最大
+      .sort({ uid: 1 }); // sort() 方法可以通过参数指定排序的字段，并使用 1 和 -1 来指定排序的方式，其中 1 为升序排列，而 -1 是用于降序排列。
     return {
       data,
       status: 200,
